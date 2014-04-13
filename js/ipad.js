03d895a030776b0cd8861be5a63e2afc8003004e
Ryan Brown <ryan@weareloring.com>
2014-04-12 23:16:47 -0700

    formatting

--- js/ipad.js
+++ js/ipad.js
@@ -1,19 +1,24 @@
-$(function(){
-    var center = Math.floor($(window).height()/2) - 100,
-        ipad_top = $('#ipad').offset().top, degrees = 90,
-        bounds = {top: center-degrees, center: center, bottom: center+degrees};
+$(function () {
+    var center = Math.floor($(window).height() / 2),
+        ipad_top = $('#ipad').offset().top,
+        degrees = 90,
+        bounds = {
+            top: center - degrees,
+            center: center,
+            bottom: center + degrees
+        };
 
-    $(window).scroll(function(event) {
+    $(window).scroll(function (event) {
         var pos = ipad_top - $(window).scrollTop();
-        if(pos < bounds.bottom && pos > bounds.top) {
-            if(pos > bounds.center) {
+        if (pos < bounds.bottom && pos > bounds.top) {
+            if (pos > bounds.center) {
                 $('img.back').css('transform', 'rotateX(90deg)');
-                $('img.front').css('transform', 'rotateX('+(bounds.bottom-pos)+'deg)');
+                $('img.front').css('transform', 'rotateX(' + (bounds.bottom - pos) + 'deg)');
             } else {
                 $('img.front').css('transform', 'rotateX(90deg)');
-                $('img.back').css('transform', 'rotateX('+(pos-bounds.top)+'deg)');
+                $('img.back').css('transform', 'rotateX(' + (pos - bounds.top) + 'deg)');
             }
-        } else if(pos > bounds.bottom) {
+        } else if (pos > bounds.bottom) {
             $('img.front').css('transform', 'rotateX(0deg)');
             $('img.back').css('transform', 'rotateX(90deg)');
         } else {
